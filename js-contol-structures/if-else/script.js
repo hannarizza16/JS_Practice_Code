80bd415a30844fa641e30a65a2810ff07c2ac8c2
hannamalana98 <hannamalana98@gmail.com>
2025-03-09 03:28:00 +0800

    if else example

--- js-contol-structures/if-else/script.js
+++ js-contol-structures/if-else/script.js
@@ -29,3 +29,46 @@ console.log("------------");
 let isRightAge5 = age >= 18 ? 'rigth' : age == 17 ? 'not right' : 'i dont know';
 // true fals
 console.log(isRightAge5);
+
+console.log("-----Example 1-------");
+
+let num = 10;
+
+if (num == 1){
+    console.log("one")
+} else {
+    console.log(num);
+}
+
+const checking = (numeral) => {
+    return numeral == 1 ? console.log("one") : console.log(numeral)
+}
+checking(num);
+
+const checking1 = (num) => {
+    return num == 10 ? console.log("ten") : console.log(num)
+}
+checking1(num);
+
+const checking2 = () => {
+    return num == 10 ? console.log("no param") : console.log(num)
+}
+checking2(num);
+
+console.log("-----Example 2-------");
+
+//  ternary 
+const age1 = 18;
+const isMinor = age < 18 ? "Minor" : "Not Minor";
+console.log(isMinor)
+
+// using function
+const isHumanMinor = (edad) => {
+    return edad < 18 ? "Minor pa" : "Matanda na";
+}
+console.log(isHumanMinor(age1));
+
+function isHumanMinor1(edad){
+    return edad < 18 ? "Minor pa" : "Matanda na talaga";
+}
+console.log(isHumanMinor1(age1));s
