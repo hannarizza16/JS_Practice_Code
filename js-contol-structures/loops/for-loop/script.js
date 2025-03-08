e90d930cd068338236b912dcfa4d956cf6ab977c
hannamalana98 <hannamalana98@gmail.com>
2025-03-09 06:02:37 +0800

    added a activity

---
+++ js-contol-structures/loops/for-loop/script.js
@@ -0,0 +1,24 @@
+// for loop 
+//initialization; condition; increment
+for ( let x = 1; x < 10; x++){
+    console.log(x);
+}
+console.log(" Example 1=== ")
+
+const number = [1, 2, 3, 4, 5];
+for (let count = 0; count < number.length; count++){
+    const numberArr = number[count];
+    console.log(`Array ${numberArr}`);
+}
+console.log(" Example 2=== ")
+
+const numbers = [1, 2, 3, 4, 5];
+for (let count = 0; count < numbers.length; count++){
+    console.log(`Array ${numbers}`);
+}
+console.log(" Example 3=== ")
+
+const numbersz = [1, 2, 3, 4, 5];
+for (let count = 0; count < numbersz.length; count++){
+    console.log(numbersz)
+}
