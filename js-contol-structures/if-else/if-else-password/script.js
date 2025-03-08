00ae718cbcf09754112c521b14c0ecb650595d5b
hannamalana98 <hannamalana98@gmail.com>
2025-03-09 03:33:38 +0800

    Simple Password Condition

---
+++ js-contol-structures/if-else/if-else-password/script.js
@@ -0,0 +1,8 @@
+let userName = "admin";
+let password = "123";
+
+if (userName == "admin" && password == "1234"){
+    alert('Login Success')
+} else {
+    alert('Login Failed, Invalid Credentials')
+}
