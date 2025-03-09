2cdcd9e8bbc989127493c934d5eed057c06e7a2f
hannamalana98 <hannamalana98@gmail.com>
2025-03-09 08:07:06 +0800

    modified

--- js-contol-structures/loops/for-loop/script.js
+++ js-contol-structures/loops/for-loop/script.js
@@ -22,3 +22,27 @@ const numbersz = [1, 2, 3, 4, 5];
 for (let count = 0; count < numbersz.length; count++){
     console.log(numbersz)
 }
+
+console.log("activity 1 =====");
+
+// *****
+// ****
+// ***
+// **
+// *
+
+for(let count = 5; count > 0; count-- ) {
+    let asterisk = "";
+    for( let num = 0; num < count; num++){
+        asterisk = asterisk + "*";
+    }
+    console.log(asterisk);
+
+}
+
+console.log("activity 2 =====");
+
+for(let count = 5; count > 0; count--){
+    console.log('*'.repeat(count));
+}
+
